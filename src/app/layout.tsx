06520120dbfdd87aca934fcc4d131deef9dc88e5
Nguyen Thi Ly <lynt@aiot-global.com>
2025-10-03 21:09:34 +0700

    fix: remove code not use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import Header from "./template/header";
 import Footer from "./template/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "My App",
